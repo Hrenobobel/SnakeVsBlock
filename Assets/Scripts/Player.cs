53b043d2d3ab9d1cde7d0f5a39b47f28a4dc25f0
Hrenobobel <elavek@mail.ru>
2021-12-19 02:03:02 +0300

    first try

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+using TMPro;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int Length = 5;
+    public TextMeshPro LengthText;
+
+    private SnakeScript SnakeScript;
+
+    void Start()
+    {
+        LengthText.SetText(Length.ToString());
+
+        SnakeScript = GetComponent<SnakeScript>();
+
+        /*for (int i = 0; i < Length; i++)
+            SnakeScript.LengthUp();*/
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Length++;
+            LengthText.SetText(Length.ToString());
+            SnakeScript.LengthUp();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Length--;
+            LengthText.SetText(Length.ToString());
+            SnakeScript.LengthDown();
+        }
+    }
+}
